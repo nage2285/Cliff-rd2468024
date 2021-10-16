1ff0e5174947a7069315534006bebf4d6f3ed229
cliffordcoco <88732726+nage2285@users.noreply.github.com>
2021-10-16 19:05:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Write your code below this row 👇
+x = 0
+for x in range (1, 101):
+    if x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    elif x % 15 == 0:
+        print("FizzBuzz")
+    else:
+        print(x)
