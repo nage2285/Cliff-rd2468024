730c593f981cb6a54d72d8e95b4bd2a43625da0b
cliffordcoco <88732726+nage2285@users.noreply.github.com>
2022-04-19 18:25:47 +0000

    15 would be divided by both 3 and 5

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #Write your code below this row 👇
 x = 0
 for x in range (1, 201):
-    if x % 3 == 0:
+    if x % 15 == 0:
+        print("FizzBuzz")
+    elif x % 3 == 0:
         print("Fizz")
     elif x % 5 == 0:
         print("Buzz")
-    elif x % 15 == 0:
-        print("FizzBuzz")
     else:
         print(x)
 # range has been added to 201
